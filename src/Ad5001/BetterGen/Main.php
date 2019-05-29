90b581c51b78bc7651f84374b51abecb9bf66bb6
JackMD <jackmtaylor.jmt@gmail.com>
2019-05-29 18:33:42 +0500

    declare strict types, type hints, reformat code

--- src/Ad5001/BetterGen/Main.php
+++ src/Ad5001/BetterGen/Main.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types = 1);
+
 /**
  *  ____             __     __                    ____
  * /\  _`\          /\ \__ /\ \__                /\  _`\
@@ -7,12 +9,12 @@
  *   \ \ \L\ \/\  __/ \ \ \_ \ \ \_ /\  __/\ \ \/  \ \ \/, \/\  __/ /\ \/\ \
  *    \ \____/\ \____\ \ \__\ \ \__\\ \____\\ \_\   \ \____/\ \____\\ \_\ \_\
  *     \/___/  \/____/  \/__/  \/__/ \/____/ \/_/    \/___/  \/____/ \/_/\/_/
+ *
  * Tomorrow's pocketmine generator.
- * @author Ad5001 <mail@ad5001.eu>, XenialDan <https://github.com/thebigsmileXD>
- * @link https://github.com/Ad5001/BetterGen
+ *
+ * @author   Ad5001 <mail@ad5001.eu>, XenialDan <https://github.com/thebigsmileXD>
+ * @link     https://github.com/Ad5001/BetterGen
  * @category World Generator
- * @api 3.0.0
- * @version 1.1
  */
 
 namespace Ad5001\BetterGen;
@@ -40,30 +42,30 @@ use pocketmine\tile\Tile;
 use pocketmine\utils\Config;
 use pocketmine\utils\Random;
 
-class Main extends PluginBase implements Listener {
-	const PREFIX = "§l§o§b[§r§l§2Better§aGen§o§b]§r§f ";
-	const SAKURA_FOREST = 100; // Letting some place for future biomes.
+class Main extends PluginBase implements Listener{
+
+	/** @var string */
+	public const PREFIX = "§l§o§b[§r§l§2Better§aGen§o§b]§r§f ";
+
+	/** @var int */
+	public const SAKURA_FOREST = 100;
 
 	/**
-	 * Regisetrs a biome to betternormal
+	 * Registers a biome to betternormal
 	 *
-	 * @param int $id
+	 * @param int   $id
 	 * @param Biome $biome
 	 * @return void
 	 */
-	public static function registerBiome(int $id, Biome $biome) {
+	public static function registerBiome(int $id, Biome $biome): void{
+		//todo figure out what the id will do... or maybe remove it?
 		BetterNormal::registerBiome($biome);
 	}
 
 	/**
-	 * Called when the plugin enables
-	 *
-	 * @return void
+	 * Save the resources first before enabling the plugin.
 	 */
-	public function onEnable() {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		GeneratorManager::addGenerator(BetterNormal::class, "betternormal");
-		@mkdir($this->getDataFolder()."loots");
+	public function onLoad(): void{
 		$this->saveResource("loots/igloo.json");
 		$this->saveResource("loots/mineshaft.json");
 		$this->saveResource("loots/temple.json");
@@ -71,125 +73,153 @@ class Main extends PluginBase implements Listener {
 	}
 
 	/**
+	 * Called when the plugin enables
+	 *
+	 * @return void
+	 */
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		GeneratorManager::addGenerator(BetterNormal::class, "betternormal");
+	}
+
+	/**
 	 * Called when a command executes
 	 *
 	 * @param CommandSender $sender
-	 * @param Command $cmd
-	 * @param int $label
-	 * @param array $args
+	 * @param Command       $cmd
+	 * @param int           $label
+	 * @param array         $args
 	 * @return bool
 	 */
-	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args): bool {
-		switch ($cmd->getName()) {
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args): bool{
+		switch($cmd->getName()){
 			case "createworld": // /createworld <name> [generator = betternormal] [seed = rand()] [options(json)]
-				switch (count($args)) {
+				switch(count($args)){
 					case 0 :
 						return false;
-						break;
+					break;
 					case 1 : // /createworld <name>
 						$name = $args[0];
 						$generator = GeneratorManager::getGenerator("betternormal");
 						$generatorName = "betternormal";
 						$seed = $this->generateRandomSeed();
 						$options = [];
-						break;
+					break;
 					case 2 : // /createworld <name> [generator = betternormal]
 						$name = $args[0];
 						$generator = GeneratorManager::getGenerator($args[1]);
-						if(GeneratorManager::getGeneratorName($generator) !== strtolower($args[1])) {
+						if(GeneratorManager::getGeneratorName($generator) !== strtolower($args[1])){
 							$sender->sendMessage(self::PREFIX . "§4Could not find generator {$args[1]}. Are you sure it is registered?");
+
 							return true;
 						}
 						$generatorName = strtolower($args[1]);
 						$seed = $this->generateRandomSeed();
 						$options = [];
-						break;
+					break;
 					case 3 : // /createworld <name> [generator = betternormal] [seed = rand()]
 						$name = $args[0];
 						$generator = GeneratorManager::getGenerator($args[1]);
-						if(GeneratorManager::getGeneratorName($generator) !== strtolower($args[1])) {
+						if(GeneratorManager::getGeneratorName($generator) !== strtolower($args[1])){
 							$sender->sendMessage(self::PREFIX . "§4Could not find generator {$args[1]}. Are you sure it is registered?");
+
 							return true;
 						}
 						$generatorName = strtolower($args[1]);
-                                                $parts = str_split($args[2]);
-                                                foreach ($parts as $key=>$str) {
-                                                        if(is_numeric($str) == false && $str <> '-'){
-                                                                $parts[$key] = ord($str);
-                                                        }
-                                                }
-                                                $seed = (int)implode("", $parts);
+						$parts = str_split($args[2]);
+						foreach($parts as $key => $str){
+							if(is_numeric($str) == false && $str <> '-'){
+								$parts[$key] = ord($str);
+							}
+						}
+						$seed = (int) implode("", $parts);
 						$options = [];
-						break;
+					break;
 					default : // /createworld <name> [generator = betternormal] [seed = rand()] [options(json)]
 						$name = $args[0];
 						$generator = GeneratorManager::getGenerator($args[1]);
-						if(GeneratorManager::getGeneratorName($generator) !== strtolower($args[1])) {
+						if(GeneratorManager::getGeneratorName($generator) !== strtolower($args[1])){
 							$sender->sendMessage(self::PREFIX . "§4Could not find generator {$args[1]}. Are you sure it is registered?");
+
 							return true;
 						}
 						$generatorName = strtolower($args[1]);
-						if ($args[2] == "rand") $args[2] = $this->generateRandomSeed();
-                                                $parts = str_split($args[2]);
-                                                foreach ($parts as $key=>$str) {
-                                                        if(is_numeric($str) == false && $str <> '-'){
-                                                                $parts[$key] = ord($str);
-                                                        }
-                                                }
-                                                $seed = (int)implode("", $parts);
+						if($args[2] == "rand"){
+							$args[2] = $this->generateRandomSeed();
+						}
+						$parts = str_split($args[2]);
+						foreach($parts as $key => $str){
+							if(is_numeric($str) == false && $str <> '-'){
+								$parts[$key] = ord($str);
+							}
+						}
+						$seed = (int) implode("", $parts);
 						unset($args[0], $args[1], $args[2]);
 						$options = json_decode($args[3], true);
-						if (!is_array($options)) {
+						if(!is_array($options)){
 							$sender->sendMessage(Main::PREFIX . "§4Invalid JSON for options.");
+
 							return true;
 						}
-						break;
+					break;
 				}
 				$options["preset"] = json_encode($options);
-				if ((int)$seed == 0/*String*/) {
+				if((int) $seed == 0/*String*/){
 					$seed = $this->generateRandomSeed();
 				}
 				$this->getServer()->broadcastMessage(Main::PREFIX . "§aGenerating level $name with generator $generatorName and seed $seed..");
 				$this->getServer()->generateLevel($name, $seed, $generator, $options);
 				$this->getServer()->loadLevel($name);
+
 				return true;
-				break;
+			break;
+
 			case "worldtp":
-				if(!$sender instanceof Player) {
+				if(!$sender instanceof Player){
 					return false;
 				}
-				if(isset($args[0])) {
-					if(is_null($this->getServer()->getLevelByName($args[0]))) {
+
+				if(isset($args[0])){
+					if(is_null($this->getServer()->getLevelByName($args[0]))){
 						$this->getServer()->loadLevel($args[0]);
-						if(is_null($this->getServer()->getLevelByName($args[0]))) {
+						if(is_null($this->getServer()->getLevelByName($args[0]))){
 							$sender->sendMessage("Could not find level {$args[0]}.");
+
 							return false;
 						}
 					}
 					$sender->teleport(Position::fromObject($sender, $this->getServer()->getLevelByName($args[0])));
 					$sender->sendMessage("§aTeleporting to {$args[0]}...");
+
 					return true;
-				} else {
+				}else{
 					return false;
 				}
 			break;
-			case 'temple':{
-				if($sender instanceof ConsoleCommandSender) return false;
-				/** @var Player $sender */
-				$temple = new Temple();
-				$temple->placeObject($sender->getLevel(), $sender->x, $sender->y, $sender->z, new Random(microtime()));
-				return true;
-			}
+
+			case 'temple':
+				{
+					if($sender instanceof ConsoleCommandSender){
+						return false;
+					}
+					/** @var Player $sender */
+					$temple = new Temple();
+					$temple->placeObject($sender->getLevel(), $sender->x, $sender->y, $sender->z, new Random(microtime()));
+
+					return true;
+				}
 		}
+
 		return false;
 	}
 
 	/**
 	 * Generates a (semi) random seed.
+	 *
 	 * @return int
 	 */
-	public function generateRandomSeed(): int {
-		return (int)round(rand(0, round(time()) / memory_get_usage(true)) * (int)str_shuffle("127469453645108") / (int)str_shuffle("12746945364"));
+	public function generateRandomSeed(): int{
+		return (int) round(rand(0, round(time()) / memory_get_usage(true)) * (int) str_shuffle("127469453645108") / (int) str_shuffle("12746945364"));
 	}
 
 	/**
@@ -197,16 +227,20 @@ class Main extends PluginBase implements Listener {
 	 *
 	 * @param string $name
 	 * @param string $treeClass
-	 * @param array $infos
+	 * @param array  $infos
 	 * @return bool
 	 */
-	public function registerForest(string $name, string $treeClass, array $infos): bool {
-		if (!@class_exists($treeClass))
+	public function registerForest(string $name, string $treeClass, array $infos): bool{
+		if(!@class_exists($treeClass)){
 			return false;
-		if (!@is_subclass_of($treeClass, "pocketmine\\level\\generator\\normal\\object\\Tree"))
+		}
+		if(!@is_subclass_of($treeClass, "pocketmine\\level\\generator\\normal\\object\\Tree")){
 			return false;
-		if (count($infos) < 2 or !is_float($infos[0]) or !is_float($infos[1]))
+		}
+		if(count($infos) < 2 or !is_float($infos[0]) or !is_float($infos[1])){
 			return false;
+		}
+
 		return BetterForest::registerForest($name, $treeClass, $infos);
 	}
 
@@ -216,13 +250,13 @@ class Main extends PluginBase implements Listener {
 	 * @param ChunkPopulateEvent $event
 	 * @return void
 	 */
-	public function onChunkPopulate(ChunkPopulateEvent $event) {
+	public function onChunkPopulate(ChunkPopulateEvent $event){
 		$cfg = new Config(LootTable::getPluginFolder() . "processingLoots.json", Config::JSON);
-		foreach ($cfg->getAll() as $key => $value) {
+		foreach($cfg->getAll() as $key => $value){
 			list($x, $y, $z) = explode(";", $key);
-			if ($value["saveAs"] == "chest" && $event->getLevel()->getBlockIdAt($x, $y, $z) == Block::AIR) {
+			if($value["saveAs"] == "chest" && $event->getLevel()->getBlockIdAt($x, $y, $z) == Block::AIR){
 				$event->getLevel()->setBlockIdAt($x, $y, $z, Block::CHEST);
-			} else {
+			}else{
 				$cfg->remove($key);
 				$cfg->save();
 			}
@@ -234,15 +268,15 @@ class Main extends PluginBase implements Listener {
 	 *
 	 * @param ChunkLoadEvent $event
 	 */
-	public function onChunkLoad(ChunkLoadEvent $event) {
-		if($event->getLevel()->getProvider()->getGenerator() === "betternormal") {
+	public function onChunkLoad(ChunkLoadEvent $event){
+		if($event->getLevel()->getProvider()->getGenerator() === "betternormal"){
 			$chunk = $event->getChunk();
-			for($x = 0; $x < 16; $x++) {
-				for($z = 0; $z < 16; $z++) {
-					for($y = 0; $y <= Level::Y_MAX; $y++) {
+			for($x = 0; $x < 16; $x++){
+				for($z = 0; $z < 16; $z++){
+					for($y = 0; $y <= Level::Y_MAX; $y++){
 						$id = $chunk->getBlockId($x, $y, $z);
 						$tile = $chunk->getTile($x, $y, $z);
-						if($id === Block::CHEST and $tile === null) {
+						if($id === Block::CHEST and $tile === null){
 							Tile::createTile(Tile::CHEST, $event->getLevel(), Chest::createNBT($pos = new Vector3($chunk->getX() * 16 + $x, $y, $chunk->getZ() * 16 + $z), null)); //TODO: set face correctly
 						}
 					}
